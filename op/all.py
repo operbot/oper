195f04c03fb8e2a585a313b38c4700a1941a2392
Bart Thate <bthate67@gmail.com>
2022-09-17 16:47:40 +0200

    add op.all

---
+++ op/all.py
@@ -0,0 +1,22 @@
+# This file is placed in the Public Domain.
+# pylint: disable=W0622,W0614,W0611,W0401,W0614,C0114
+
+
+import op.cls
+import op.dbs
+import op.dft
+import op.jsn
+import op.obj
+import op.sel
+import op.utl
+import op.wdr
+
+
+from op.cls import *
+from op.dbs import *
+from op.dft import *
+from op.jsn import *
+from op.obj import *
+from op.sel import *
+from op.utl import *
+from op.wdr import *
