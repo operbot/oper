b20b540f2b74ce4514eb6d0bd418bfc61f3b8e3f
operbot <operbot100@gmail.com>
2022-09-23 17:00:38 +0200

    update opr

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     description="operator bot",
     long_description=read(),
     license="Public Domain",
-    packages=["op", "opr", "oper"],
+    packages=["op", "or", "om"],
     scripts=["bin/op", "bin/oper"],
     classifiers=[
         "Development Status :: 4 - Beta",
